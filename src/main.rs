077673db96ca277a67d573c88909fb530d168a27
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-10 23:57:37 +0430

    Cleanup main file

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ extern crate postgres;
 mod service;
 mod router;
 mod flare;
-/// Async request handler. Ddb pool is stored in application state.
-
 
 fn main() -> io::Result<()> {
     flare.spark();
