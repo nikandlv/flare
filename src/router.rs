274555d845a66f88e733b87bb7701c23336ea88b
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-13 00:00:03 +0430

    Removed unresolved route

--- src/router.rs
+++ src/router.rs
@@ -1,11 +1,8 @@
   
 use actix_web::{web};
 
-use crate::service::index;
-
 pub fn get(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/")
-            .route("/{name}", web::get().to(index::handle))
     );
 }
